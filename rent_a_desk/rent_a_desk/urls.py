4f42d7c52722deb27286402d43cc61c5aeeea8a1
Vlad Nikitin <nikitinv91@gmail.com>
2021-12-27 13:53:53 +0300

    static root added

--- rent_a_desk/rent_a_desk/urls.py
+++ rent_a_desk/rent_a_desk/urls.py
@@ -1,6 +1,8 @@
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path, re_path
 
@@ -9,7 +11,7 @@ urlpatterns = [
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 # API Documentation
 schema_view = get_schema_view(
