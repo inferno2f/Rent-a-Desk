c41c087fbaa9b28d4b2fa8124dfb4ddc75e0a7a6
Vlad Nikitin <nikitinv91@gmail.com>
2021-12-27 11:55:21 +0300

    collect static settings added

--- rent_a_desk/rent_a_desk/settings.py
+++ rent_a_desk/rent_a_desk/settings.py
@@ -1,9 +1,19 @@
 from pathlib import Path
 
 import django_heroku
+import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 # Quick-sart development settings - unsuitable for production
